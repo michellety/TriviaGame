1824a9583873914d96f14d1bdb0cad61b5977938
michelle <michelletyson29@gmail.com>
2019-01-20 14:50:27 -0800

    pseudocode to outline the basic game requirements

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//have code run only when the page is ready
+
+//array containing the question, multiple answer options, and the correct answer 
+
+//variables for timing, and score keeping
+
+//start button to begin the game
+
+//hide start button
+
+//start timer and display the remaining time 
+
+//show first question and answer options
+
+//click event to store the users answer
+
+//compare the users answer to the correct answer
+
+//move to the next question
+
+//reset the timer
+
+//after the last question, show the final results
+
+
